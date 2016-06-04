66ae609aeee6909e81014ddf8b978b1fd0ea3a86
Kaito <Kaito@VAIO-KAITO>
2016-06-05 08:50:51 +0900

    プロジェクト ファイルを追加します。

---
+++ 血液型占い/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+                MessageBox.Show("慎重");
+            else if (radioButton2.Checked)
+                MessageBox.Show("マイペース");
+            else if (radioButton3.Checked)
+                MessageBox.Show("おおらか");
+            else if (radioButton4.Checked)
+                MessageBox.Show("天才肌");
+        }
+    }
+}
